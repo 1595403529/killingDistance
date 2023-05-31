2bb6b200aa15ccc06c16a6312d120fa0eb320e3b
1595403529 <13730100881@163.com>
2023-05-31 16:31:30 +0800

    Initial commit

---
+++ src/main/java/com/xiaobu/killDistance/KillDistanceApplication.java
@@ -0,0 +1,13 @@
+package com.xiaobu.killdistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KillDistanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KillDistanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaobu/killDistance/KillDistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaobu.killdistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KillDistanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
